71c483505ac4e461ffead99369c0ac021662d2c5
Alexandr Karavsky <ak@encode.dk>
2020-02-10 18:36:07 +0200

    graphql external api

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,34 +1,30 @@
 const graphql = require('graphql');
+const fetch = require("node-fetch");
 
 const {
     GraphQLObjectType,
     GraphQLString,
-    GraphQLInt,
+    GraphQLFloat,
     GraphQLSchema
 } = graphql;
 
-const users = [
-    {id: 1, firstName: 'Bob', age: 44},
-    {id: 2, firstName: 'Jay', age: 43}
-]
-
-const UserType = new GraphQLObjectType({
-    name: 'MyUserTypeName',
+const StockType = new GraphQLObjectType({
+    name: 'Day',
     fields: {
-        id:{ type: GraphQLInt},
-        firstName: {type: GraphQLString},
-        age: {type: GraphQLInt }
+        date: { type: GraphQLString },
+        open: { type: GraphQLFloat },
+        close: { type: GraphQLFloat },
     }
 });
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
-        user: {
-            type: UserType,
-            args: { id: { type: GraphQLInt } },
+        day: {
+            type: StockType,
+            args: { date: { type: GraphQLString } },
             resolve(parentValue, args) {
-                return users.find(user => user.id === args.id);
+                return fetch('https://financialmodelingprep.com/api/v3/historical-price-full/index/^DJI').then(res => res.json()).then(json => json).then(prices => prices.historical.find(day => day.date === args.date));
             }
         }
     }
@@ -40,11 +36,10 @@ module.exports = new GraphQLSchema({
 
 /*
 query
-
 {
-  user(id: 1) {
-  	id,
-  	age
+  day(data: "2020-01-03") {
+  	open,
+  	close
 	}
 }
 */
