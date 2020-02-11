7fb0b918d19623ed467d5f9bd133a7b9641c75bb
Alexandr Karavsky <ak@encode.dk>
2020-02-11 17:32:22 +0200

    nested queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,19 +1,44 @@
 const graphql = require('graphql');
 const fetch = require("node-fetch");
 
+const stocks = {
+    1: {
+        name: 'Apple'
+    },
+    2: {
+        name: 'Google'
+    }
+};
+
+
 const {
     GraphQLObjectType,
     GraphQLString,
+    GraphQLInt,
     GraphQLFloat,
     GraphQLSchema
 } = graphql;
 
-const StockType = new GraphQLObjectType({
+const MostValuebleStocksType = new GraphQLObjectType({
+    name: 'MostValuebleStocks',
+    fields: {
+        date: { type: GraphQLString },
+        name: { type: GraphQLString }
+    }
+});
+
+const DayType = new GraphQLObjectType({
     name: 'Day',
     fields: {
         date: { type: GraphQLString },
         open: { type: GraphQLFloat },
         close: { type: GraphQLFloat },
+        mostValuebleStocksType: {
+            type: MostValuebleStocksType,
+            resolve(parentValue, args) {
+               return stocks[parentValue.mostValuebleStocksId];
+            }
+        }
     }
 });
 
@@ -21,14 +46,18 @@ const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
         day: {
-            type: StockType,
+            type: DayType,
             args: { date: { type: GraphQLString } },
             resolve(parentValue, args) {
-                return fetch('https://financialmodelingprep.com/api/v3/historical-price-full/index/^DJI').then(res => res.json()).then(json => json).then(prices => prices.historical.find(day => day.date === args.date));
+                return fetch('https://financialmodelingprep.com/api/v3/historical-price-full/index/^DJI').then(res => res.json()).then(json => json).then(prices => {
+                    const day = prices.historical.find(day => day.date === args.date);
+                    day.mostValuebleStocksId = 1;
+                    return day
+                });
             }
         }
     }
-})
+});
 
 module.exports = new GraphQLSchema({
     query: RootQuery
@@ -37,7 +66,7 @@ module.exports = new GraphQLSchema({
 /*
 query
 {
-  day(data: "2020-01-03") {
+  day(date: "2020-01-03") {
   	open,
   	close
 	}
