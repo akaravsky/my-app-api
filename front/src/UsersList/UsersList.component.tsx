3126452304bc19956fa4d4383052f52cacc81793
Alexandr Karavsky <askaravskiy@gmail.com>
2020-03-26 22:41:54 +0200

    graphql add front libraries

---
+++ front/src/UsersList/UsersList.component.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+const UsersList = () => {
+    return ( <div>
+        <div>Users</div>
+        <img src="http://lorempixel.com/400/400" />
+    </div>)
+}
+
+const query = gql`
+
+`;
+
+export default UsersList
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -7,49 +7,66 @@ import {
     Link
 } from "react-router-dom";
 
+import ApolloClient, { ApolloClientOptions } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
 const Home = lazy(() => import('./Home'));
 //const Users = lazy(() => import('./Users.jsx'));
 const About = lazy(() => import('./About'));
 
-import Users from './Users'
+import UsersList from './UsersList/UsersList.component'
 
 import './index.css';
 
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:3000/'
+});
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache,
+  link
+});
+
 const App = () => {
     return (
-        <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/users">Users</Link>
-                        </li>
-                    </ul>
-                </nav>
+        <ApolloProvider client={client}>
+            <Router>
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">About</Link>
+                            </li>
+                            <li>
+                                <Link to="/users">Users</Link>
+                            </li>
+                        </ul>
+                    </nav>
 
-                {/* A <Switch> looks through its children <Route>s and
+                    {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-                <Suspense fallback={<div>Loading...</div>}>
-                    <Switch>
-                        <Route path="/about">
-                            <About />
-                        </Route>
-                        <Route path="/users">
-                            <Users />
-                        </Route>
-                        <Route path="/">
-                            <Home props1={'MyHome'} props2={1}/>
-                        </Route>
-                    </Switch>
-                </Suspense>
-            </div>
-        </Router>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Switch>
+                            <Route path="/about">
+                                <About />
+                            </Route>
+                            <Route path="/users">
+                                <UsersList />
+                            </Route>
+                            <Route path="/">
+                                <Home props1={'MyHome'} props2={1} />
+                            </Route>
+                        </Switch>
+                    </Suspense>
+                </div>
+            </Router>
+        </ApolloProvider>
     )
 }
 
