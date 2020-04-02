ca335aea94c2fcfd6c1bd588b8fe2cc4da0309a3
Alexandr Karavsky <ak@encode.dk>
2020-04-02 19:18:09 +0300

    non relative paths

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -15,11 +15,11 @@ const config = {
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".jsx"],
-        /*modules: [
-            path.resolve('./src'), //for non relative paths
-            path.resolve('./node_modules')
-        ]*/
+        modules: [
+            './src', //for non relative paths
+            "./node_modules"
+        ],
+        extensions: [".ts", ".tsx", ".js", ".jsx"]
     },
     module: {
         rules: [
