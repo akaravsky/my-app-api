503a3b48fd6bb462217dd80ccbee62a50162bee7
Alexandr Karavsky <askaravskiy@gmail.com>
2020-09-16 17:04:21 +0300

    split apollo and router logic

---
+++ front/src/Router.tsx
@@ -0,0 +1,52 @@
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Header from './Header.component';
+
+const Home = lazy(() => import('./Home'));
+//const Users = lazy(() => import('./Users.jsx'));
+const About = lazy(() => import('./About'));
+
+import CreateUserForm from 'UsersPage/CreateUserForm/CreateUserForm.component';
+import EditUserForm from 'UsersPage/EditUserForm/EditUserForm.component';
+import UsersPage from 'UsersPage/UsersPage.component';
+import Companies from 'CompaniesPage/CompaniesPage.component';
+import CreateCompanyForm from 'CompaniesPage/CompaniesList/CreateCompanyForm/CreateCompanyForm.component';
+
+const Router = (): JSX.Element => {
+    const [tab, setTab] = React.useState();
+    return (
+        <BrowserRouter>
+            <Header tab={tab} setTab={setTab} />
+            {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                    <Route path="/users/new">
+                        <CreateUserForm setTab={setTab} />
+                    </Route>
+                    <Route path="/users/:id">
+                        <EditUserForm setTab={setTab} />
+                    </Route>
+                    <Route path="/users">
+                        <UsersPage />
+                    </Route>
+                    <Route path="/companies/new">
+                        <CreateCompanyForm setTab={setTab} />
+                    </Route>
+                    <Route path="/companies">
+                        <Companies />
+                    </Route>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/">
+                        <Home props1={'MyHome'} props2={1} />
+                    </Route>
+                </Switch>
+            </Suspense>
+        </BrowserRouter>
+    );
+};
+
+export default Router;
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,23 +1,12 @@
-import React, { Suspense, lazy } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 
-import Header from './Header.component';
-
-const Home = lazy(() => import('./Home'));
-//const Users = lazy(() => import('./Users.jsx'));
-const About = lazy(() => import('./About'));
-
-import CreateUserForm from 'UsersPage/CreateUserForm/CreateUserForm.component';
-import EditUserForm from 'UsersPage/EditUserForm/EditUserForm.component';
-import UsersPage from 'UsersPage/UsersPage.component';
-import Companies from 'CompaniesPage/CompaniesPage.component';
-import CreateCompanyForm from 'CompaniesPage/CompaniesList/CreateCompanyForm/CreateCompanyForm.component';
+import Router from './Router';
 
 const cache = new InMemoryCache({
     dataIdFromObject: (o): string | undefined => o.id //we can add some identifier that helps apollo know which component should be updated after changing
@@ -32,41 +21,9 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
 });
 
 const App = (): JSX.Element => {
-    const [tab, setTab] = React.useState();
-
     return (
         <ApolloProvider client={client}>
-            <Router>
-                <Header tab={tab} setTab={setTab} />
-
-                {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-                <Suspense fallback={<div>Loading...</div>}>
-                    <Switch>
-                        <Route path="/users/new">
-                            <CreateUserForm setTab={setTab} />
-                        </Route>
-                        <Route path="/users/:id">
-                            <EditUserForm setTab={setTab} />
-                        </Route>
-                        <Route path="/users">
-                            <UsersPage />
-                        </Route>
-                        <Route path="/companies/new">
-                            <CreateCompanyForm setTab={setTab} />
-                        </Route>
-                        <Route path="/companies">
-                            <Companies />
-                        </Route>
-                        <Route path="/about">
-                            <About />
-                        </Route>
-                        <Route path="/">
-                            <Home props1={'MyHome'} props2={1} />
-                        </Route>
-                    </Switch>
-                </Suspense>
-            </Router>
+            <Router />
         </ApolloProvider>
     );
 };
