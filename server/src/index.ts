20672e0fa09b57327f02e5278b565ffc69dd9949
Alexandr Karavsky <askaravskiy@gmail.com>
2020-11-24 10:03:26 +0200

    docker

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,6 +20,19 @@ corsInit(app);
 authInit(app);
 graphqlInit(app);
 
+if(process.env.NODE_ENV === 'production') {
+  //Express will serve up production assets
+  //like our main.js file, or main.css file
+  app.use(express.static('../front/build'))
+
+  //Express will serve up the index.html file
+  //if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req: any, res: any) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+}
+
 function passportInit(app: any) {
   // Passport is wired into express as a middleware. When a request comes in,
   // Passport will examine the request's session (as set by the above config) and
