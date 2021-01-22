86fdd7a60f7d7fdeadb062dab0b99d7659d2b96e
Alexandr Karavsky <askaravskiy@gmail.com>
2021-01-22 09:26:34 +0200

    fix

--- src/helpers/corsInit.ts
+++ src/helpers/corsInit.ts
@@ -3,7 +3,7 @@ const cors = require("cors");
 export const corsInit = (app: any) => {
   app.use(
     cors({
-      origin: ["http://localhost:8080"],
+      origin: ["http://localhost:8080", "http://localhost:3050"],
       credentials: true,
     })
   );
